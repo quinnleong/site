47f19130a74fc88af4669dfd0f4599141d95f313
Quinn Leong <quinn@honeycomb.io>
2026-02-17 21:19:20 -0600

    Content

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,9 +11,17 @@ module.exports = {
       transparent: 'transparent',
       white: '#ffffff',
       primary: colors.stone,
+      // footer was otherwise just `bg-primary-500/10`, using default tailwind colors (stone, defined right above this)
+      footer: '#d19f87',
+      // bg: '#db9877',
+      bg: '#e0b49f',
+      // header: '#7ec0cb',
+      header: '#a9bbbe',
+      button: '#835746'
     },
     fontFamily: {
-      sans: ['Inter', 'sans-serif'],
+      sans: ['Palatino', 'sans-serif'],
+      header: ['DM Serif Text', 'sans-serif']
     },
     fontSize: {
       xs: ['0.75rem', '1rem'],
@@ -21,11 +29,15 @@ module.exports = {
       base: ['1rem', '1.75rem'],
       lg: ['1.125rem', '2rem'],
       xl: ['1.25rem', '2.125rem'],
+      '1xl': ['1.35rem', '2rem'],
       '2xl': ['1.5rem', '2rem'],
       '3xl': ['1.875rem', '2.375rem'],
       '4xl': ['2.25rem', '2.75rem'],
       '5xl': ['3rem', '3.5rem'],
       '6xl': ['3.75rem', '4.25rem'],
+    },
+    size: {
+      '9': {width: '3rem', height: '3rem' }
     },
   },
 };
